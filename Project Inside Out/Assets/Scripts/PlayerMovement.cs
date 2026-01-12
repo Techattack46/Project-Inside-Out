e41c43109e5ec09a9084c31132e91d97c3a666f0
Techattack46 <bigvyg@gmail.com>
2026-01-12 19:21:18 +0100

    tried to improve jumping, but no success

--- Project Inside Out/Assets/Scripts/PlayerMovement.cs
+++ Project Inside Out/Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,20 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D playerBody;
+    public float groundYPosition;
+    public bool isJumping = false;
+
+    private void Start()
+    {
+        isJumping = false;
+    }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
         }
@@ -16,5 +24,17 @@ public class PlayerMovement : MonoBehaviour
     {
         Debug.Log("Jumping now.");
         playerBody.AddForce(Vector2.up, ForceMode2D.Impulse);
+        isJumping = true;
+
+        //GroundCheck();
+    }
+
+    private void GroundCheck()
+    {
+        if(gameObject.transform.position.y <= groundYPosition)
+        {
+            Debug.Log("Landed again.");
+            isJumping = false;
+        }
     }
 }
