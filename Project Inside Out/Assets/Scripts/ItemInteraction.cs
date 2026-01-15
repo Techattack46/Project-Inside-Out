aea50cde98ba611535ebd905f1ba8e1e030479e0
Techattack46 <bigvyg@gmail.com>
2026-01-15 17:04:13 +0100

    fixed item equipping on wrong side

--- Project Inside Out/Assets/Scripts/ItemInteraction.cs
+++ Project Inside Out/Assets/Scripts/ItemInteraction.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class ItemInteraction : MonoBehaviour
 {
     public GameObject player;
+    public SpriteRenderer playerSprite;
     public Vector2 offset;
     public bool lockedToPlayer;
 
@@ -12,9 +13,18 @@ public class ItemInteraction : MonoBehaviour
     {
         if (lockedToPlayer)
         {
-            GetComponent<BoxCollider2D>().excludeLayers = LayerMask.GetMask("Player");
-
-            transform.position = (Vector2) player.transform.position + offset;
+            if (playerSprite.flipX)
+            {
+                transform.position = new Vector2(
+                    player.transform.position.x - offset.x,
+                    player.transform.position.y + offset.y);
+                
+                //transform.position = (Vector2)player.transform.position - offset; //except only x needs to be flipped
+            }
+            else
+            {
+                transform.position = (Vector2)player.transform.position + offset;
+            }
         }
     }
 }
